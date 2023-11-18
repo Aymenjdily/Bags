c08b5bc0f9f650b773c721304f59aaeaf68de578
Aymen Jdily <87182808+Aymenjdily@users.noreply.github.com>
2023-11-18 13:22:20 +0100

    setup projects

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
+import '@radix-ui/themes/styles.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../globals.css'
+import { Theme } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme>
+          <main>
+            {children}
+          </main>
+        </Theme>
+      </body>
     </html>
   )
 }
